f3b4b415fe4d27ac71251636a2b03898cc62e11a
mmcampbell04 <meghan@cygnet.io>
2025-10-20 00:15:02 +0100

    update utils for prod

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,3 +1,2 @@
-// export const API_BASE_URL = "http://localhost:8000";
-
-export const API_BASE_URL = "/api";
+export const API_BASE_URL =
+  "/choreo-apis/choose-your-own-adventure/backend/v1/api";
